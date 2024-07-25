187f6abad4cdd7648bb9c9ef3b244f90993bf430
CG <tinykidtoodotcom@yahoo.com>
2024-07-24 23:21:43 -0700

    Add change item for multi state items

--- Assets/Scripts/PlayerScripts/Inventory/Item.cs
+++ Assets/Scripts/PlayerScripts/Inventory/Item.cs
@@ -9,21 +9,6 @@ public class Item : MonoBehaviour
     public ItemScriptableObject itemData;
     [HideInInspector] public Transform locationOverride;
 
-    //[Header("Inspect item settings")]
-    //[SerializeField] UnityEvent onInspect;
-
-    //[Header("Pickup item settings")]
-    //[SerializeField] float pickupRadius = 1.25f;
-    //float forcePickupAfter = 5f;
-    //Transform player;
-    //bool pickUpWhenNear;
-    //[SerializeField] UnityEvent onPickup;
-
-    //[Header("Talking item settings")]
-    //[SerializeField] DialogueScript dialogueScript;
-    //[SerializeField] CinemachineVirtualCamera virtualCamera;
-    //[SerializeField] UnityEvent onTalk;
-
     [HideInInspector] public InspectAction inspectAction;
     [HideInInspector] public TalkAction talkAction;
     [HideInInspector] public PickupAction pickupAction;
@@ -43,46 +28,10 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (pickUpWhenNear && Vector3.Distance(player.position, transform.position) < pickupRadius) 
-        //    PickInWorldItem();
 
     }
 
-    //public void Inspect() {
-    //    DialogueUI.Instance.DisplayGrannyText(itemData.inspectDialogue);
-    //    onInspect.Invoke();
-    //}
-
-    #region Pickup
-    //private void PickInWorldItem() {
-    //    gameObject.SetActive(false);  //TODO this should happen when granny gets there or another things is pickedup
-    //}
-
-
-
-    //public void PickUp() {
-    //    if (pickUpWhenNear) return;
-
-    //    player = FindObjectOfType<PlayerMovement>().transform;
-
-    //    Inventory.Instance.AddItem(itemData);
-
-    //    pickUpWhenNear = true;
-    //    Invoke(nameof(PickInWorldItem), forcePickupAfter);
-
-    //    onPickup.Invoke();
-    //}
-    #endregion
-
-    #region Talking
-    //public void TalkToObject() {
-    //    GameManager.Instance.EnterTalkingMode(virtualCamera.transform, this, dialogueScript.scriptList);
-    //    onTalk.Invoke();
-    //}
-    #endregion
-
-    //private void OnDrawGizmosSelected() {
-    //    if(itemData.takeable)
-    //        Gizmos.DrawWireSphere(transform.position, pickupRadius);
-    //}
+    public void ChangeItemScriptableObject(ItemScriptableObject item) {
+        itemData = item;
+    }
 }
