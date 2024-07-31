c1d4bff6f39e8a290b7dea160c401e37737f22e8
CG <tinykidtoodotcom@yahoo.com>
2024-07-31 00:40:35 -0700

    Puzzle 3 now puzzles, no solution animations

---
+++ Assets/Scripts/PuzzleScripts/Car3Solution.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Car3Solution : MonoBehaviour
+{
+    bool isColdTime = false;
+    bool isFireBurning = false;
+
+    [SerializeField] UnityEvent OnSolved;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetColdTemp() {
+        isColdTime = true;
+    }
+
+    public void LightFire() {
+        isFireBurning = true;
+    }
+
+    public void PressDial() {
+        if (isColdTime && isFireBurning) {
+            OnSolved.Invoke();
+        }
+        else {
+            if (!isColdTime)
+                DialogueUI.Instance.DisplayGrannyText("I don't think the waters cold enough");
+            else if (!isFireBurning)
+                DialogueUI.Instance.DisplayGrannyText("Somethings missing, its not cold enough, whats that hanging thing?");
+        }
+    }
+}
