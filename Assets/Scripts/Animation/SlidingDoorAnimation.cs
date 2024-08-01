49bdb66b222176eff41e63477a67c9f01e192372
CG <tinykidtoodotcom@yahoo.com>
2024-08-01 14:52:00 -0700

    Fix sliding doors

--- Assets/Scripts/Animation/SlidingDoorAnimation.cs
+++ Assets/Scripts/Animation/SlidingDoorAnimation.cs
@@ -22,12 +22,18 @@ public class SlidingDoorAnimation : MonoBehaviour
         CloseDoor();
     }
 
+    private void Update() {
+        if(Input.GetKey(KeyCode.L)) {
+            OpenDoor();
+        }
+    }
+
     public void OpenDoor() {
         if(isOpen) { return; }
 
         tween?.Kill(true);
         isOpen = true;
-        tween = doorTransform.DOMove(openPosition, duration);
+        tween = doorTransform.DOMove(closePosition - openPosition, duration);
 
         Invoke(nameof(CloseDoor), openTime);
     }
@@ -38,5 +44,12 @@ public class SlidingDoorAnimation : MonoBehaviour
         tween?.Kill(true);
         isOpen = false;
         tween = doorTransform.DOMove(closePosition, duration);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position + closePosition - openPosition, 0.15f);
+        Gizmos.DrawWireSphere(transform.position + closePosition, 0.15f);
+        Gizmos.DrawLine(transform.position + closePosition - openPosition, transform.position + closePosition);
     }
 }
