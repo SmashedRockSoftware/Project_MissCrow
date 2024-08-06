79dd52efc1624d17cb3e299a4ed05fde51d66c8c
CG <tinykidtoodotcom@yahoo.com>
2024-08-06 14:31:56 -0700

    moved some fake lights, testing swinging

--- Assets/Scripts/Graphics/swingingAnimation.cs
+++ Assets/Scripts/Graphics/swingingAnimation.cs
@@ -5,17 +5,31 @@ using DG.Tweening;
 
 public class swingingAnimation : MonoBehaviour {
     [SerializeField] Transform swingingTransform;
+    [SerializeField] private Vector3 startRotation;
     [SerializeField] private Vector3 endRotation;
     [SerializeField] private float duration = 0.25f;
+    [SerializeField] private float variance = 0.25f;
     Tween tween;
 
     [SerializeField] Ease ease = Ease.InOutSine;
+
+    bool goEnd = true;
 
     //[SerializeField] AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start() {
-        tween = swingingTransform.DORotate(endRotation, duration).SetLoops(-1, LoopType.Yoyo).SetEase(ease);
+        //tween = swingingTransform.DORotate(endRotation, duration).SetLoops(-1, LoopType.Yoyo).SetEase(ease);
+        if(startRotation == Vector3.zero) startRotation = transform.position;
+        DoRandomRotateTime();
+    }
+
+    public void DoRandomRotateTime() {
+        var randDuration = duration + Random.Range(-variance, variance);
+        var rotateTo = goEnd ? endRotation : startRotation;
+        goEnd = !goEnd;
+
+        tween = swingingTransform.DORotate(rotateTo, randDuration).SetEase(ease).OnComplete(() => { DoRandomRotateTime(); });
     }
 
     // Update is called once per frame
