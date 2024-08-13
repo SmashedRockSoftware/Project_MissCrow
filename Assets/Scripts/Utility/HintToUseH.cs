5b5a567718bc959f33d0df63f594212f0975c14d
CG <tinykidtoodotcom@yahoo.com>
2024-08-12 19:13:23 -0700

    Added click help text

--- Assets/Scripts/Utility/HintToUseH.cs
+++ Assets/Scripts/Utility/HintToUseH.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HintToUseH : MonoBehaviour
 {
+    private const string HSTRHELPER = "Need help? Hold \"H\" to highlight everything in a puzzle.";
+    private const string INTERHELPER = "Click to interact";
     [SerializeField] float maxHintTime = 30f;
     [SerializeField] float timeTillHint = 5f;
     [SerializeField] float delayHintWhenPressed = 2f;
     [SerializeField] float timer;
 
-    [SerializeField] GameObject hintText;
+    [SerializeField] TextMeshProUGUI hintText;
 
     [Space]
     [SerializeField] GameObject comboHint;
@@ -18,11 +21,21 @@ public class HintToUseH : MonoBehaviour
     void Start()
     {
         timer = timeTillHint;
+        hintText.text = INTERHELPER;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(GameManager.Instance.currentGameState != GameState.InGame) {
+            hintText.gameObject.SetActive(false);
+            return;
+        }
+
+        if(Input.GetMouseButtonDown(0) && hintText.text == INTERHELPER) {
+            hintText.text = HSTRHELPER;
+        }
+
         timer -= Time.deltaTime;
 
         hintText.gameObject.SetActive(timer < 0);
